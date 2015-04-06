940d2e3be9a8c368b3fd1437ec4c9edbb06cd90c
Alex Stupakov <stupakov@gmail.com>
2015-04-05 17:38:42 -0700

    show full version of elements when zoomed > 2x

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,4 +1,6 @@
 $(function() {
+  var scale; // the current scale level
+
   var zoom = d3.behavior
   .zoom()
   .scaleExtent([1, 8])
@@ -8,7 +10,7 @@ $(function() {
   .call(zoom);
 
   function zoomed() {
-    var scale = d3.event.scale;
+    scale = d3.event.scale;
     var translateX = d3.event.translate[0];
     var translateY = d3.event.translate[1];
 
@@ -19,6 +21,22 @@ $(function() {
       "-webkit-transform" : transformString,
       "-ms-transform" : transformString
     });
+
+    $('.node-container').each(function(_, element) {
+      $element = $(element);
+      if(elementZoomedAboveThreshold(element)) {
+        $element.addClass("full").removeClass("summary");
+      } else {
+        $element.addClass("summary").removeClass("full");
+      };
+    })
+  }
+
+  var elementZoomedAboveThreshold = function($element) {
+    var threshold = 2;
+    var elementScaleX = $element.getBoundingClientRect().width / $element.offsetWidth;
+    return elementScaleX > threshold;
+    // return scale > threshold; // shortcut if all things are considered to have the same zoom level
   }
 });
 
