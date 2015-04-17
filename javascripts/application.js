55c72febdd105eb591a0a4a63ab9858943d27762
Alex Stupakov <astupakov@pivotal.io>
2015-04-16 21:59:12 -0700

    Use custom html elements to identify summary and full versions
    - no longer use divs to surround groups of elements that define the
    summary and full versions of each node
    - use Web Components Custom Elements instead
    - these have 'display: inline-block' and their behavior is defined in js
    - We'll need to use polyfills to add support for other browsers

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,44 +1,88 @@
+var setUpCustomElements = function() {
+
+  var chooseVariantToShow = function(node, summarize) {
+    if(summarize) {
+      node.showSummary();
+    } else {
+      node.showFull();
+    }
+  };
+
+  // TODO simplify boolean attribute - http://stackoverflow.com/questions/9201471/how-to-add-boolean-attribute-using-javascript
+  var dataNodeProto = Object.create(HTMLElement.prototype, {
+    createdCallback: {
+      value: function() {
+        this.summaryElement = this.querySelector('data-summary:first-of-type');
+        this.fullElement = this.querySelector('data-full:first-of-type');
+
+        chooseVariantToShow(this, this.getAttribute("summarize") !== "false");
+      }
+    },
+    attributeChangedCallback: {
+      value: function(attrName, oldVal, newVal) {
+        if(attrName === "summarize") {
+          chooseVariantToShow(this, newVal !== "false")
+        }
+      }
+    },
+    showSummary: {
+      value: function() {
+        this.summaryElement.classList.remove("data-node-hidden");
+        this.fullElement.classList.add("data-node-hidden");
+      }
+    },
+    showFull: {
+      value: function() {
+        this.fullElement.classList.remove("data-node-hidden");
+        this.summaryElement.classList.add("data-node-hidden");
+      }
+    }
+  });
+  document.registerElement('data-node', {prototype: dataNodeProto});
+}
+
+
 $(function() {
-  var scale; // the current scale level
+  var documentScale; // the current scale level
 
   var zoom = d3.behavior
   .zoom()
-  .scaleExtent([1, 8])
+  .scaleExtent([1, 16])
   .on("zoom", zoomed);
 
   var container = d3.select(".outer-container")
   .call(zoom);
 
   function zoomed() {
-    scale = d3.event.scale;
+    documentScale = d3.event.scale;
     var translateX = d3.event.translate[0];
     var translateY = d3.event.translate[1];
 
-    var transformString = "translate(" + translateX + "px," + translateY + "px) scale(" + scale + ")";
+    var transformString = "translate(" + translateX + "px," + translateY + "px) scale(" + documentScale + ")";
 
+    // TODO use a library to handle multi-browser css
     $('.container').css({
       "transform" : transformString,
       "-webkit-transform" : transformString,
       "-ms-transform" : transformString
     });
 
-    $('.node-container').each(function(_, element) {
-      $element = $(element);
-      if(elementZoomedAboveThreshold(element)) {
-        $element.addClass("full").removeClass("summary");
+    $('data-node').each(function(idx, element) {
+      if(elementZoomedAboveThreshold(idx, element)) {
+        element.showFull();
       } else {
-        $element.addClass("summary").removeClass("full");
+        element.showSummary();
       };
     })
   }
 
-  var elementZoomedAboveThreshold = function(element) {
+  var elementZoomedAboveThreshold = function(idx, element) {
     var threshold = 2;
 
     var elementScaleX = element.getBoundingClientRect().width / element.offsetWidth;
-
     return elementScaleX > threshold;
-    // return scale > threshold; // shortcut if all things are considered to have the same zoom level
   }
+
+  setUpCustomElements();
 });
 
